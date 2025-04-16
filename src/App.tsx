10cc0d871440c45a11d927362f942e03b858db29
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-04-16 15:26:43 -0300

    Create CountDown component.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./styles/theme.css";
 import { Container } from "./components/Container";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 export function App() {
   return (
@@ -14,6 +15,10 @@ export function App() {
 
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
