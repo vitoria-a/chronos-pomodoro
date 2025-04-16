5ac7928cca4a20e9cb627924934e9cdcff48d3cb
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-04-16 14:46:03 -0300

    Organize project structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,19 @@
 import "./styles/global.css";
 import "./styles/theme.css";
 
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
+
 export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
 
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>FORM</section>
-        </div>
-      </div>
-
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
