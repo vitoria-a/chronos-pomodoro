2316b978c98ccc4e4c947ceded04c0e2e1842226
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-04-17 15:10:38 -0300

    Create and style Cycles component.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import { Input } from "./components/Input";
+import { Cycles } from "./components/Cycles";
 
 export function App() {
   return (
@@ -36,8 +37,7 @@ export function App() {
             <p>Lorem ipsum dolors sit amet.</p>
           </div>
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0</p>
+            <Cycles />
           </div>
           <div className="formRow">
             <button>Enviar</button>
