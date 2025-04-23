978cafee34a1cf297f1a16a2cbfa45adf3f5ab32
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-04-23 10:29:40 -0300

    Using essential state hooks is done to save theme data to localStorage.

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,20 +1,80 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { useState, useEffect } from "react";
 import styles from "./styles.module.css";
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from "lucide-react";
+
+type AvailableThemes = "dark" | "light";
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem("theme") as AvailableThemes) || "dark";
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+  ) {
+    event.preventDefault();
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+
+    return () => {
+      localStorage.setItem("theme", theme);
+    };
+  }, [theme]);
+
   return (
     <div className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Go to Home"
+        title="Go to Home"
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="View history"
+        title="View history"
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Settings"
+        title="Settings"
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href="#">
-        <SunIcon />
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Change theme"
+        title="Change theme"
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
       </a>
     </div>
   );
