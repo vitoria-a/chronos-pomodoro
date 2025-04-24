765786f284252ac7b0caaad10de9e4c3a644f25a
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-04-24 09:24:07 -0300

    Implement Context API concept (useContext + useState).

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./styles/global.css";
 import "./styles/theme.css";
 import { Home } from "./pages/Home";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 
 export function App() {
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
