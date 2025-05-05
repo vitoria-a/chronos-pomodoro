c4e0f2ed592ac9a2861f3ee9c7d7c3fe737344ef
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-05-05 09:32:01 -0300

    Create MainRouter component and apply ScrollTop to pages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import "./styles/global.css";
 import "./styles/theme.css";
-import { Home } from "./pages/Home";
 import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 import { MessagesContainer } from "./components/MessagesContainer";
-import { BrowserRouter, Route, Routes } from "react-router";
-import { NotFound } from "./pages/NotFound";
-import { AboutPomodoro } from "./pages/AboutPomodoro";
+import { MainRouter } from "./routers/MainRouter";
 
 export function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+        <MainRouter />
       </MessagesContainer>
     </TaskContextProvider>
   );
