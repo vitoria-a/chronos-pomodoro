65f2d5b48e247a8dcc0a37db745aae122cd8e89c
Vitória Alves <vitoriagomes@alu.ufc.br>
2025-05-05 12:19:42 -0300

    Capture the onSubmit of the form with ref and useRef of the inputs on the Settings page.

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,8 +4,25 @@ import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { useRef } from "react";
 
 export function Settings() {
+  const { state } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+  function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const workTime = workTimeInput.current?.value;
+    const shortBreakTime = shortBreakTimeInput.current?.value;
+    const longBreakTime = longBreakTimeInput.current?.value;
+
+    console.log(workTime, shortBreakTime, longBreakTime);
+  }
+
   return (
     <MainTemplate>
       <Container>
@@ -19,15 +36,30 @@ export function Settings() {
       </Container>
 
       <Container>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={handleSaveSettings}>
           <div className="formRow">
-            <Input id="workTime" labelText="Focus" />
+            <Input
+              defaultValue={state.config.workTime}
+              labelText="Focus"
+              id="workTime"
+              ref={workTimeInput}
+            />
           </div>
           <div className="formRow">
-            <Input id="shortBreakTime" labelText="Short rest" />
+            <Input
+              defaultValue={state.config.shortBreakTime}
+              labelText="Short rest"
+              id="shortBreakTime"
+              ref={shortBreakTimeInput}
+            />
           </div>
           <div className="formRow">
-            <Input id="longBreakTime" labelText="Long rest" />
+            <Input
+              defaultValue={state.config.longBreakTime}
+              labelText="Long rest"
+              id="longBreakTime"
+              ref={longBreakTimeInput}
+            />
           </div>
           <div className="formRow">
             <Button
